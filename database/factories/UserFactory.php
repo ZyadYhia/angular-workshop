5f20df99e9af0a15cd5415e7057e285a8a14b9ac
ZyadYhiaJisr <z.yhia@jisr.net>
2025-11-30 10:02:37 +0200

    revert to fake()

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'username' => $this->faker->unique()->userName(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'phone_number' => $this->faker->phoneNumber(),
+            'name' => fake()->name(),
+            'username' => fake()->unique()->userName(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone_number' => fake()->phoneNumber(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
